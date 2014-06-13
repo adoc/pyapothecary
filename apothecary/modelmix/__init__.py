06bebecca9104a5cd7f0a3179b03c059679ac8bf
adoc <adoc@code.webmob.net>
2014-06-13 16:53:58 -0700

    Additional mixins added/refactored.

--- apothecary/modelmix/__init__.py
+++ apothecary/modelmix/__init__.py
@@ -11,10 +11,14 @@ __all__ = (idmix, IdMix, tsmix, TsMix)
 
 
 def _timefunc():
+    """
+    """
     return int(time.time())
 
 
 def idmix(id_key='id'):
+    """
+    """
     class IdMix(object):
         """Integer primary key model mixin."""
         pass
@@ -23,42 +27,50 @@ def idmix(id_key='id'):
 IdMix = idmix()
 
 
-def tsmix(created_key='ts_created', updated_key='ts_updated', defer=True,
+def tsmix(ts_col, oncreate=False, onupdate=False, defer=False,
           timefunc=_timefunc, Type=sqlalchemy.types.Integer):
+    """
+    `ts_col` - Column name for created timestamp.
+    `oncreate` -
+    `onupdate` _
+    `defer` - Defer loading of the columns to access time rather
+              than at query.
+    `timefunc` - 
+    `Type` -
+    """
     class TsMix(object):
-        """Base Timestamp model mixin.
-        `created_key` - Column name for created timestamp.
-        `updated_key` - Column name for updated timestamp.
-        `defer` - Defer loading of the columns to access time rather
-                  than at query."""
+        """Timestamp Model Mixin.
+        """
         pass
 
-    def created_col(cls):
-        return sqlalchemy.Column(Type, default=timefunc,
-                                 nullable=False)
-
-    def updated_col(cls):
-        return sqlalchemy.Column(Type, default=timefunc,
-                                 onupdate=timefunc, nullable=False)
+    def col(**col_kwa):
+        # Create the Column object with various options.
+        if oncreate is True:
+            col_kwa['default'] = timefunc
+        if onupdate is True:
+            col_kwa['default'] = timefunc #??
+            col_kwa['onupdate'] = timefunc
+        col_kwa['nullable'] = not(oncreate or onupdate)
+        return sqlalchemy.Column(Type, **col_kwa)
 
     if defer is true:
-        setattr(TsMix, created_key, sqlalchemy.orm.defered(created_col()))
-        setattr(TsMix, updated_key, sqlalchemy.orm.defered(updated_col()))
+        # Defer loading of column.
+        setattr(TsMix, ts_col, sqlalchemy.orm.defered(col()))
     else:
-        setattr(TsMix, created_key, created_col())
-        setattr(TsMix, updated_key, updated_col())
-
+        # Eager (?) loading of column.
+        setattr(TsMix, ts_col, col())
     return TsMix
-TsMix = tsmix()
+TsUpdatedMix = tsmix('ts_updated', onupdate=True)
+TsCreatedMix = tsmix('ts_created', oncreate=True)
 
 
-def flagmix(flag_attr='a_flag', default=True, col_name=None,
-            invert_filter=False, Type=sqlalchemy.types.Boolean):
+def flagmix(flag_col, default=True, invert_filter=False,
+            Type=sqlalchemy.types.Boolean):
     """Allows for the arbitrary creation of flag (boolean) attributes.
     Queries are automatically filtered based on the flag when coupled
     with .unique_constructor, .query_filters, or .rebase metaclass.
 
-    `flag_attr` - the name of the flag attribute.
+    `flag_col` - the name of the flag attribute.
         This is also doubles as the table column name unless
         `col_name` is set. Two methods are added based on this name:
             flag_[flagattr] - Sets the flag. (alias of FlagMix._flag)
@@ -68,19 +80,21 @@ def flagmix(flag_attr='a_flag', default=True, col_name=None,
 
     `invert_filter` - This inverts the filter from expecting a True
         value to Fale.
+
+    `Type` - The SQLAlchemy type to use for the column.
     """
     # Removed the confusion of `inverse`. Replaced with `invert_filter`
     # Removed col_name.
     class FlagMix(object):
         """ """
-        @property
+        @hybrid_property
         def _flag(self):
-            return getattr(self, flag_attr)
+            return getattr(self, flag_col)
 
         @_flag.setter
         def _flag(self, value):
             assert isinstance(value, bool), "`value` must be boolean."
-            setattr(self, flag_attr, value)
+            setattr(self, flag_col, value)
 
         def _set_flag(self):
             self._flag = True
@@ -96,12 +110,77 @@ def flagmix(flag_attr='a_flag', default=True, col_name=None,
         return sqlalchemy.Column(sqlalchemy.types.Boolean, default=default,
                                  nullable=False)
 
-    setattr(FlagMix, '_'.join(['set', flag_attr]), FlagMix._set_flag)
-    setattr(FlagMix, '_'.join(['unset', flag_attr]), FlagMix._unset_flag)
-    setattr(FlagMix, flag_attr, flag_col())
+    setattr(FlagMix, '_'.join(['set', flag_col]), FlagMix._set_flag)
+    setattr(FlagMix, '_'.join(['unset', flag_col]), FlagMix._unset_flag)
+    setattr(FlagMix, flag_col, flag_col())
 
     return FlagMix
-
 FlagMix = flagmix()
 ActiveMix = flagmix('active')
 DeletableMix = flagmix('deleted', default=False, invert_filter=True)
+
+
+def flagtsmix(flag_col, ts_col, default_flag=False, timefunc=_timefunc):
+    FlagMix = flagmix(flagcol, default=False)
+    TsMix = tsmix(ts_col)
+
+    class FlagTsMix(FlagMix, TsMix):
+        """
+        """
+        # Might be needed if setter decorator isn't available in this scope.
+        #@hybrid_property
+        #def _flag(self):
+        #    return FlagMix._flag(self)
+
+        @_flag.setter
+        def _flag(self, value):
+            # Can this be refactored to not repeat what's in FlagMix?
+            assert isinstance(value, bool), "`value` must be boolean."
+            setattr(self, flag_col, value)
+
+            if value is True:
+                setattr(self, ts_col, timefunc())
+            else:
+                setattr(self, ts_col, None)
+
+    return FlagTsMix
+
+
+def record_token_mix(token_col, length=6, oncreate=False, onupdate=False,
+                     tokenfunc=_tokenfunc, index=True,
+                     Type=sqlalchemy.types.String):
+    """
+    """
+    if length < 6:
+        log.warning("SecMix with a length less than 6 is not recomended.")
+
+    class RecordTokenMix(object):
+        """
+        """
+        @hybrid_property
+        def _token(self):
+            return getattr(self, token_col)
+
+        @_token.setter
+        def _token(self, value):
+            assert isinstance(value, basestring), "`value` must be a string."
+            setattr(self, token_col, value)
+
+        def revoke_token(self):
+            """
+            """
+            self._token = tokenfunc()
+
+    def col(**kwa):
+        # Create the Column object with various options.
+        if oncreate is True:
+            col_kwa['default'] = tokenfunc
+        if onupdate is True:
+            col_kwa['default'] = tokenfunc #??
+            col_kwa['onupdate'] = tokenfunc
+        col_kwa['nullable'] = not(oncreate or onupdate)
+        col_kwa['index'] = index
+        return sqlalchemy.Column(Type(length), **col_kwa)
+
+    setattr(RecordTokenMix, token_col, col())
+    return RecordTokenMix
